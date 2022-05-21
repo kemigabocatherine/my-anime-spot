9613e19152ff2e3ffe3ca50bc4ecff162d916025
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-05-21 15:46:38 +0300

    Add browser router and styles file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/configureStore';
+import './styles/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
