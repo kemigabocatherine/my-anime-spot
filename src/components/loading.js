dd6ee3177689b666cf764b4460d50aeef14dccd8
Kemigabo Catherine <kemigabocatherine205@gmail.com>
2022-05-21 15:39:32 +0300

    Fetch and display all animes,add menu for selecting animes

---
+++ src/components/loading.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const LoadingElement = () => (
+  <div className="loading-container">
+    <h2> Getting your animes </h2>
+    <div>
+      <iframe
+        title="naruto running"
+        src="https://gifer.com/embed/4Kj3"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
+  </div>
+);
+
+export default LoadingElement;
